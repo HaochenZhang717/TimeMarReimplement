3feda640fd408243d21dc1c929d42f25dc7f2a28
Haochen Zhang <hz112@rice.edu>
2026-02-05 12:49:20 -0600

    0205_dual_vqvae

---
+++ train_dual_vqvae.sh
@@ -0,0 +1,8 @@
+DATA="stock"
+python train_dual_vqvae.py \
+--data ${DATA} \
+--config "configs/train_vq_${DATA}.yaml" \
+--device "cpu" \
+--max_epochs 5000 \
+--val_every 100 \
+--save_dir "dual_vqvae_save_dir"
