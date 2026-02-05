90d31aca012d5b85c1fd6c8a3499b1762da52c38
Haochen Zhang <hz112@rice.edu>
2026-02-05 12:50:42 -0600

    0205_dual_vqvae

--- train_dual_vqvae.sh
+++ train_dual_vqvae.sh
@@ -2,7 +2,7 @@ DATA="stock"
 python train_dual_vqvae.py \
 --data ${DATA} \
 --config "configs/train_vq_${DATA}.yaml" \
---device "cpu" \
+--device "cuda" \
 --max_epochs 5000 \
 --val_every 100 \
 --save_dir "dual_vqvae_save_dir"
